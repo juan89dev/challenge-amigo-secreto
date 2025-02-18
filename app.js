ca1ba85938083742368223867dea871d0ef37a5a
juan89dev <juan.zapataz@cun.edu.co>
2025-02-18 12:25:15 -0500

    Update app.js, implementación de lista y función para agregar nombres

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+let amigos = [];
+
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById("amigo").value;
+    if (nombreAmigo == "") {
+        alert("Por favor, ingrese un nombre");
+        /* return; */
+    } else {
+        amigos.push(nombreAmigo);
+        alert("Amigo agregado");
+        };
+        
+    document.getElementById("amigo").value = "";
+
+    console.log(amigos);
+}
